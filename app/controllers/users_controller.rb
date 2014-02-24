5b4cc62c9740813027e43c9cf223bb604ad3cff6
David de Bie <daviddebie09@gmail.com>
2014-02-24 21:59:20 +0100

    Add SSL in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   end
   def create
   	@user = User.new(user_params)
-  	if User.save
+  	if @user.save
+  	  flash[:success] = "Welcome to the Sample App"
   	  redirect_to @user
   	else
   	  render 'new'
