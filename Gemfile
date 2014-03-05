83096317fd79d3b97e9edd30bf82ea1a148b71ac
David de Bie <daviddebie09@gmail.com>
2014-03-05 13:29:46 +0100

    Finish user update, edit, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,8 +4,11 @@ ruby '1.9.3'
 gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
 gem 'protected_attributes'
 gem 'turbolinks'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/edit',    to: 'users#edit',           via: 'get'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
